bb4bbb9c417538d29096f12b464cb85ef0994b69
Camilo Morales <camiloandres.1509@gmail.com>
2020-04-02 10:50:05 -0500

    Creating place

--- models/city.py
+++ models/city.py
@@ -3,6 +3,8 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from models.place import Place
 
 
 class City(BaseModel, Base):
@@ -14,3 +16,4 @@ class City(BaseModel, Base):
     __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship('Places', backref='cities')
--- models/user.py
+++ models/user.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """This is the user class"""
 from models.base_model import BaseModel, Base
+from models.place import Place
 from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
@@ -17,3 +19,4 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
+    places = relationship('Place', backref='user')
