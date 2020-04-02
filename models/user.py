a3e5c65fe6f38edb890a2d0590fdd498ef2d5e39
Camilo Morales <camiloandres.1509@gmail.com>
2020-04-02 09:21:14 -0500

    Adding the user class

--- models/user.py
+++ models/user.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """This is the user class"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This is the class for user
     Attributes:
         email: email address
