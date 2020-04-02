64602af5d1dc5e69f98ac4945e3c46c207eca09d
Camilo Morales <camiloandres.1509@gmail.com>
2020-04-01 21:20:04 -0500

    Uploading task 6

--- models/__init__.py
+++ models/__init__.py
@@ -8,7 +8,13 @@ from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
+from os import getenv
+from models.engine.db_storage import DBStorage
 
 
-storage = FileStorage()
-storage.reload()
+if getenv("HBNB_TYPE_STORAGE") == 'db':
+    storage = DBStorage()
+    storage.reload()
+else:
+    storage = FileStorage()
+    storage.reload()
--- models/state.py
+++ models/state.py
@@ -1,11 +1,28 @@
 #!/usr/bin/python3
 """This is the state class"""
-from models.base_model import BaseModel
+from models import storage
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.city import City
+from os import getenv
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """This is the class for State
     Attributes:
         name: input name
     """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    if getenv("HBNB_TYPE_STORAGE" == 'db'):
+        cities = relationship("City", backref='state')
+    else:
+        @property
+        def cities(self):
+            city = storage.all()
+            mylist = []
+            for i in city:
+                if i.states_id == self.id:
+                    mylist.append(i)
+            return mylist
