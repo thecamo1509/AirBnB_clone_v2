b3779d8af72a7a4b94cfba99c4da5a0a914d9865
Camilo Morales <camiloandres.1509@gmail.com>
2020-04-02 11:08:49 -0500

    Updating the place class

--- models/place.py
+++ models/place.py
@@ -5,7 +5,7 @@ from sqlalchemy import Column, Integer, String, ForeignKey, Float
 from sqlalchemy.orm import relationship
 
 
-class Place(BaseModel):
+class Place(BaseModel, Base):
     """This is the class for Place
     Attributes:
         city_id: city id
