8462bd228255c78d4069af568b1c24f051b9b611
Camilo Morales <camiloandres.1509@gmail.com>
2020-04-10 12:15:20 -0500

    Finally it worked

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+from fabric.api import local, run, env, put, sudo
+from datetime import datetime
+from os.path import exists
+env.hosts = ["3.84.104.13", "35.237.21.147"]
+
+
+def do_deploy(archive_path):
+    """This function will deploy the web static to the servers"""
+    if exists(archive_path) is False:
+        return False
+    try:
+        put(archive_path, "/tmp/")
+        filename = archive_path.split("/")[1].split(".")[0]
+        path = "/data/web_static/releases/{}".format(filename)
+        run("sudo mkdir -p {}/".format(path))
+        run("sudo tar -zxf /tmp/{}.tgz -C {}/".format(filename, path))
+        run("sudo rm /tmp/{}".format(archive_path.split("/")[1]))
+        run("sudo mv /data/web_static/releases/{}/web_static/*\
+        /data/web_static/releases/{}".format(filename, filename))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -sf /data/web_static/releases/{}\
+        /data/web_static/current".format(filename))
+        print("New version deployed!")
+        return True
+    except Exception:
+        return False
