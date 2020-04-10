4912a26a7e4b8bf6c43ad7262d8b5231a813bd31
Camilo Morales <camiloandres.1509@gmail.com>
2020-04-10 17:11:46 -0500

    Trying boolean expression

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -10,6 +10,6 @@ def do_clean(number=0):
     """Deletes the useless versions"""
     number = int(number)
     local("ls -d -1tr versions/* | tail -n +{} | \
-          xargs -d '\n' rm -f --".format(number < 1 ? 2: number + 1))
+          xargs -d '\n' rm -f --".format(2 if number < 1 else number + 1))
     run("ls -d -1tr /data/web_static/releases/* | tail -n +{} | \
-          xargs -d '\n' rm -rf --".format(number < 1 ? 2: number + 1))
+          xargs -d '\n' rm -rf --".format(2 if number < 1 else number + 1))
