a61f6b28d18ec886c7624b7f0d9323cbf63c1f68
Camilo Morales <camiloandres.1509@gmail.com>
2020-04-21 12:31:09 -0500

    fixing message

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def hello():
     """This is the hello function"""
-    return 'Hello HBNB'
+    return 'Hello HBNB!'
 
 
 if __name__ == '__main__':
