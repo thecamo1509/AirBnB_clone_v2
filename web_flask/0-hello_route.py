b72bbae5bf63518983fbfe2d06db262a20604c20
Camilo Morales <camiloandres.1509@gmail.com>
2020-04-21 12:26:28 -0500

    Uploading the first hello world

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from flask import Flask
+app = Flask(__name__)   
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """This is the hello function"""
+    return 'Hello HBNB'
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000)
